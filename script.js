a76bbc912135ee2049889314359ed450ae8b975d
Matthew South <matthewsouth@Matthews-MacBook-Pro.local>
2025-05-14 08:21:04 +0900

    update css

--- script.js
+++ script.js
@@ -15,21 +15,3 @@ function carousel() {
   x[myIndex - 1].classList.add("showing");
   setTimeout(carousel, 3000); //change every 3 seconds
 }
-
-const changeLang = {};
-let myIndex = 0;
-carousel();
-
-function carousel() {
-  let i;
-  let x = document.querySelectorAll(".mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].classList.remove("showing");
-  }
-  myIndex++;
-  if (myIndex > x.length) {
-    myIndex = 1;
-  }
-  x[myIndex - 1].classList.add("showing");
-  setTimeout(carousel, 3000); //change every 3 seconds
-}
